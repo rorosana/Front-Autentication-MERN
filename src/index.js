b8904aa6785dc71d54ea35d0fd4c0024b8ae2dfb
rorosana <r.a.caraballo8@gmail.com>
2023-03-13 00:04:15 +0100

    all

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import App from "./App";
+import React from "react";
+//import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom';
+
+//ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById('root')).render(<App />);
+
